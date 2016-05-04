bc1d66e1017683b4589f672fa0540b8b0fb78e38
Pravin Kandala <pravinkandala@hotmail.com>
2016-05-03 23:54:12 -0500

    Event Registration Page UI update

---
+++ app/src/main/java/edu/niu/cs/z1761257/gathr/EventRegistration.java
@@ -0,0 +1,13 @@
+package edu.niu.cs.z1761257.gathr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventRegistration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_registration);
+    }
+}
--- app/src/main/java/edu/niu/cs/z1761257/gathr/MainActivity.java
+++ app/src/main/java/edu/niu/cs/z1761257/gathr/MainActivity.java
@@ -1,13 +1,32 @@
 package edu.niu.cs.z1761257.gathr;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+
+public class MainActivity extends Activity {
+
+    public static final String YOUR_APPLICATION_ID = "i18cYcUxV5dF0aoT0Ohg9g1pMcccVed6DBIgGneG";
+    public static final String YOUR_CLIENT_KEY = "9mwRnblLdjQC7cBCAVPj7GYKdZ8vCrvf3DfyERRl";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
+
+        // Test creation of object
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
     }
 }
